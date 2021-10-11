fc7b3aa9c5068e7317d17fb31a17c36447fc02ac
mestru17 <mestru17@student.aau.dk>
2021-10-11 23:37:48 +0200

    Add argument validator unit tests

--- src/args/validate.rs
+++ src/args/validate.rs
@@ -36,3 +36,60 @@ pub fn uint(s: String) -> Result<(), String> {
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn length_zero() {
+        let f = length(0, 0);
+        assert!(f(String::from("")).is_ok());
+        assert!(f(String::from("too long")).is_err());
+    }
+
+    #[test]
+    fn length_normal() {
+        let f = length(2, 5);
+        assert!(f(String::from("1")).is_err());
+        assert!(f(String::from("12")).is_ok());
+        assert!(f(String::from("123")).is_ok());
+        assert!(f(String::from("1234")).is_ok());
+        assert!(f(String::from("12345")).is_ok());
+        assert!(f(String::from("123456")).is_err());
+    }
+
+    #[test]
+    fn pushover_key_length() {
+        assert!(pushover_key(String::from("tooshortopasdfghjklzxcvbnm012")).is_err());
+        assert!(pushover_key(String::from("validlengthsdfghjklzxcvbnm0123")).is_ok());
+        assert!(pushover_key(String::from("toolongggthsdfghjklzxcvbnm01234")).is_err());
+    }
+
+    #[test]
+    fn pushover_key_characters() {
+        assert!(pushover_key(String::from("invalidiop!sdfghjklzxcvbnm0123")).is_err());
+        assert!(pushover_key(String::from("invalidiopasdfghj&lzxcvbnm0123")).is_err());
+        assert!(pushover_key(String::from("invalid#¤%&/()=?@£$€xcvbnm0123")).is_err());
+        assert!(pushover_key(String::from("qwertyuiopasdfghjklzxcvbnm0123")).is_ok());
+    }
+
+    #[test]
+    fn uint_length() {
+        assert!(uint(String::from("")).is_err());
+        assert!(uint(String::from("1")).is_ok());
+        assert!(uint(String::from("12")).is_ok());
+        assert!(uint(String::from("1234")).is_ok());
+        assert!(uint(String::from("12345678")).is_ok());
+        assert!(uint(String::from("1234567890123456789")).is_ok());
+        assert!(uint(String::from("12345678901234567890")).is_err());
+    }
+
+    #[test]
+    fn uint_characters() {
+        assert!(uint(String::from("invalid")).is_err());
+        assert!(uint(String::from("01234!6789")).is_err());
+        assert!(uint(String::from("012#456789")).is_err());
+        assert!(uint(String::from("0123456789")).is_ok());
+    }
+}
